4684ae160d7f35062e428d4ac11f778c9992e2af
xty669 <1879862262@qq.com>
2023-06-19 21:45:55 +0800

    更新 测试用.py

--- 测试用.py
+++ 测试用.py
@@ -1,7 +1,9 @@
 i=(input("请输入名字"))
 p=(input("请输入职业"))
 n=int(input("请输入年龄"))
-print("你是",i,"职业",p,"年龄",n)
+print("名字",i)
+print("职业",p)
+print("年龄",n)
 if n==24:
     print("你是一个一个一个")
     print("还有就是你是有意的还是不小心的")
